f1af1afcc13439ba4071e9eb6d6ba2c42383744b
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-07-04 16:13:49 +0530

    created variables
    added some variables in my code

--- Week 1/String.py
+++ Week 1/String.py
@@ -1,1 +1,18 @@
-print("hello")
+# Printing the welcome message 
+message = 'Hello World' 
+#Make variable name as descriptive as possible
+
+#we can use the escape character to print the single quote in our string
+message_with_quote = 'It\'s a beautiful day!'
+
+#we can also use double quotes to avoid using the escape character to add the single quote
+message_with_double_quotes = "It's a beautiful day!"
+
+#we can also use triple quotes to add multiple lines in our string
+message_with_triple_quotes = """Using triple quote we can add multiple lines like this in our string and the triple quotes can be either single or double quotes"""
+
+#printing all the variables
+print(message)
+print(message_with_quote)
+print(message_with_double_quotes)
+print(message_with_triple_quotes)
