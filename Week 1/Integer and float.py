b4b528e55d9349ca772e36b36025955fc2a72693
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-07-07 13:18:00 +0530

    added some more codes

--- Week 1/Integer and float.py
+++ Week 1/Integer and float.py
@@ -1,1 +1,22 @@
-"""hi"""
+num = 45
+num_float = 45.6
+
+print(type(num))
+print(type(num_float))
+
+
+#Arithmetic operations on integers and floats
+print("\n---Arithmetic Operations---")
+
+a = 34
+b = 3
+c = 2
+d =3
+
+print("Addition : ",a+b)    #Addition
+print("Subtraction : " ,a - b)  #Subtraction
+print("Multiplication : " ,a*b)  #Multiplication
+print("Division : ", a/b)   #Division(float Result)
+print("Floor Division : " ,)  #Division(integer Result)
+print("Modulus :  ",a%b)  #Modulus (give remainder of the Division)
+print("Exponent : ", c**d)  #Exponent (gives the power of the number)
