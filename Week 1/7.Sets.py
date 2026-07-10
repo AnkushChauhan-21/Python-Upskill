9dfcde600201fe4efcaaf544d56aa67707278dd6
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-07-10 23:33:33 +0530

    added some codes in the sets part
    some code is still left to do in the next commit

---
+++ Week 1/7.Sets.py
@@ -0,0 +1,24 @@
+comp_science_courses = {"DSA" , "computer fundamentals" , "operating system" ,"sql" , "advance java"}
+
+print("\n-- printing the original set comp_science_courses ",comp_science_courses)
+#the output will not be in order as sets dont care about the order everytime we print the set it will give the different order everytime 
+
+
+# Adding the element in the set
+comp_science_courses.add("python")
+print("\n-- printing the set with added element in the set",comp_science_courses)
+
+
+
+# removing the element in the set
+comp_science_courses.remove("python")
+print("\n--printing the set after removing the one element from it : ",comp_science_courses)
+
+
+# sets are unordered collection with not duplicate elements
+dup_set = {"23" , "money"  , "unauthorized","banana","unauthorized"}
+print("\n printing the set with duplicate elements here : ",dup_set)
+
+
+
+
