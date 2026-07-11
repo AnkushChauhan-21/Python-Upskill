ded60dea238c4dde9c3a605658db442e4c89c084
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-07-11 16:06:57 +0530

    added the operations of the sets

--- Week 1/7.Sets.py
+++ Week 1/7.Sets.py
@@ -20,5 +20,31 @@ dup_set = {"23" , "money"  , "unauthorized","banana","unauthorized"}
 print("\n printing the set with duplicate elements here : ",dup_set)
 
 
+courses = {"DSA","operating system","sql","Hindi","english"}
+print("\n---printing this set too : ",courses)
+
+
+
+# Set operations
+
+print("\n---Sets operations here--")
+print("\nunion of two sets comp_ or courses : ",comp_science_courses.union(courses)) #union of the two sets here (combines the both sets together)
+
+print("\n now intersection between the two sets (comp_ and courses) : ", comp_science_courses.intersection(courses))
+"""
+this will show the intersection of the two sets which are mentioned above intersection  is basically the common elements that are present in the both of the sets so the printed set will has the common elements of the both sets and nothing else
+"""
+
+print("\ndifference ( comp_ - courses) : ", comp_science_courses.difference(courses)) # it means show the elements present in the comp_ but not the elements of the courses set 
+
+print("\n difference (courses - comp_) : ",courses.difference(comp_science_courses)) # in this we want the elements of the courses but not the elements present in the comp_ set 
+
+print("\nsymmetric difference--- : ",comp_science_courses.symmetric_difference(courses))
+# symmetric difference means the all the elements will be printed in both sets but not the intersection elements means the common elements in both the sets will not be printed 
+
+
+
+
+
 
 
