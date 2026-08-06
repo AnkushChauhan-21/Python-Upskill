0da202f47d1d6d5eb2a6a31aede48e13874e8fa6
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-08-06 10:20:18 +0530

    added new assignement

---
+++ Assignment/week 1 assignment/Assignment 2(input and output).py
@@ -0,0 +1,15 @@
+print("---Task 1 ------")
+admin_name = input("enter Your name admin : ").upper()
+print(f"Welcome Mr/Mrs {admin_name} \n Good Morning , shall we start with todays work")
+
+print("---task 2 ----")
+num1 = float(input("enter the desired number for calculation : "))
+num2 = float(input("enter the desired number for calculation : "))
+sum = num1 + num2
+multi = num1 * num2
+division = num1 / num2
+print(f"so the sum of {num1} and {num2} is {sum}")
+print(f"so the multiplication of {num1} and {num2} is {multi}")
+print(f"so the division of the {num1} and {num2} is {division}")
+
+print(--task 3 ----)
