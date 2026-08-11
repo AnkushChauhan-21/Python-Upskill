8aaca93d7a82f488781d31dfa37c8f55fe51dbae
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-08-11 20:23:46 +0530

    completed some more function might need to start the revision now as we are moving towards the project

--- Practice/Week 2/Built-in functions.py
+++ Practice/Week 2/Built-in functions.py
@@ -32,3 +32,39 @@ print(ages)
 print("All age are above 25 : " ,all(age >= 18  for age in ages))
 print("At least one is above 25 : ",any(age >=25 for age in ages))
 
+# Numpy Examples
+import numpy as np
+array = np.array([[1,2,3] , [3,45,6] , [2,4,5]])
+print("The Original Array is : ",array)
+print("All elements in the array are non-zero : ",np.all(array))
+print("At least one element in the array is non-zero : ",np.any(array))
+
+
+# Lets Take the example of the power levels 
+power_level = np.array([6000,4500,7000,45000,9000,10000])
+print("The Original Array is : ",power_level)
+print("All power levels exceed 8000 : ",np.all(power_level > 8000))
+print("At least one power level exeed 9000 : ",np.any(power_level > 9000))
+
+
+# Enumerate function
+# The enumerate() function adds an counter to an iterable and return it as the enumerate  object
+# Syntax : enumerate(iterable,start=0)
+
+print("\n---- lets print the enumerate function here ----")
+fruits = ["Apple" , "mango" , "banana"]
+
+for index , fruit in enumerate(fruits):
+    print(f"Index {index} : {fruit}")
+
+
+
+new_index = ["Apple" , "mango" , "banana"]
+for index , fruit in enumerate(new_index, start=1):
+    print(f"\n Index {index} : {fruit}")
+
+
+
+
+
+
