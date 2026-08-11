690295caaf2087f58c5fee4f7d525d1d1d1981ca
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-08-11 16:57:49 +0530

    done with any and all functions)

---
+++ Practice/Week 2/Built-in functions.py
@@ -0,0 +1,30 @@
+# all and any functions
+# all() : Returns True if all the elements in the iterables are True,(or if the iterable is empty) 
+# any() : Returns True if any element in the iterable is True ,  but return False if the iterable is empty
+
+
+boolean = [True,False,True,False]
+string = ["" ,"Money","honey" ]
+numbersss = [0,1,2,3]
+
+# lets test and see the all() and any() on boolean list
+print("\n The Original List : ",boolean)
+print("All elements are True : ",all(boolean))
+print("At least one element is True : ",any(boolean))
+
+
+
+# lets try all() and any() on num list
+print("\n The Original List : ",numbersss)
+print("All elements are non-zero : ",all(numbersss))
+print("At least one element is non-zero  : ",any(numbersss))
+
+
+# Lets try all() and any() on string list
+print("\n The Original List : ",string)
+print("All strings are non-empty : ",all(string))
+print("At least one string is non empty : ",any(string))
+
+
+#
+
