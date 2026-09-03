730ca282c641ab7a8d4b44e2eca46d5b3b6ed58f
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-09-03 19:47:50 +0530

    done

--- Assignment/Week 2 assignment/Assignment 6(Decoraters).py
+++ Assignment/Week 2 assignment/Assignment 6(Decoraters).py
@@ -72,5 +72,62 @@ print(int(sqr_root(144)))
 # Task 4
 print("\n------ Task 4 ------\n")
 
+cache_structure = {}
+def cache(func):
+    def wrapper (*args , **kwargs):
+        value = cache_structure.get(args[0])
+        print(f"The value of the cache_structure is {value} for {args[0]}")
+        if(value is None):
+            print(f"Data for the {args[0]} Not Found!!!!")
+            value = func(*args , **kwargs)
+            cache_structure[args[0]]=value
+        return value
+    return wrapper
 
+
+
+@cache
+def doing_computation(x):
+    print("The computation is being done here")
+    return x * x
+
+
+print(doing_computation(5))
+print(doing_computation(5))
+print(doing_computation(7))
+print(doing_computation(7))
         
+
+
+
+
+# Task 5
+print("\n------ Task 5 ------\n")
+
+def require_permission(func):
+    def wrapper(*args , **kwargs):
+        user_name = args[0]['Name']
+        user_permission = args[0]['Permission']
+        print(f"Checking permissions for {user_name} with permission level {user_permission}")
+        if(user_permission.count('Admin')>0):
+            func(*args , **kwargs)
+        else:
+            print("Access Denied")
+    return wrapper
+
+
+
+
+@require_permission
+def delete_user(user , userId):
+    print(f"User {userId} deleted the {user['Name']}")
+
+
+user1 ={ "Name" : "Ankush" , "Permission" : "Admin"}
+user2 ={"Name" : "Ankush" , "Permission" : "Dev"}
+user3 ={"Name" : "Ankush" , "Permission" : "user"}
+
+
+user_list= [user1,user2,user3]
+for index , user in enumerate(user_list):
+    delete_user(user , index+1)
